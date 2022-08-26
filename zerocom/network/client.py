b775b7988acfd2a399cbc1405dbb3048deade9cf
ItsDrike <itsdrike@protonmail.com>
2022-08-26 10:09:21 +0200

    Add basic client implementation

---
+++ zerocom/network/client.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import asyncio
+from typing import TYPE_CHECKING
+
+from zerocom.network.connection import Connection
+from zerocom.packets import read_packet, write_packet
+from zerocom.packets.ping import Ping, Pong
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+
+class Client:
+    def __init__(self, server_address: tuple[str, int], timeout: float, connection: Connection):
+        self.address = server_address
+        self.timeout = timeout
+        self.connection = connection
+
+    @classmethod
+    async def create(cls, server_address: tuple[str, int], timeout: float) -> Self:
+        conn = asyncio.open_connection(server_address[0], server_address[1])
+        reader, writer = await asyncio.wait_for(conn, timeout=timeout)
+        connection = Connection(reader, writer, timeout)
+        return cls(server_address, timeout, connection)
+
+    async def connect(self) -> None:
+        print("Sending ping request..")
+        packet = Ping("myrandomtoken")
+        await write_packet(self.connection, packet)
+        resp_packet = await read_packet(self.connection)
+
+        if not isinstance(resp_packet, Pong):
+            raise Exception("...")
+
+        print("Got pong!")
+        if resp_packet.token != "myrandomtoken":
+            raise Exception("not match")
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(self, *args, **kwargs) -> None:
+        self.connection.close()
