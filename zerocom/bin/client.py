9582f39dbca3f0901ec7faf100b30c217cc2eea6
ItsDrike <itsdrike@protonmail.com>
2022-08-27 13:04:15 +0200

    Add server and client scripts into bin/

---
+++ zerocom/bin/client.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from zerocom.config import Config
+from zerocom.network.client import Client
+
+
+async def main() -> None:
+    client = await Client.create((Config.IP, Config.PORT), timeout=3)
+
+    async with client:
+        await client.connect()
+        await asyncio.sleep(50)
+        await client.connect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ zerocom/bin/server.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from zerocom.config import Config
+from zerocom.network.server import Server
+
+
+async def main() -> None:
+    server = await Server.create((Config.IP, Config.PORT), timeout=float("inf"))
+    await server.listen()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
